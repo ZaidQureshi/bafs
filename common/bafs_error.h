f2f2269ae279d3c45ac14a1c76eec438a2672b66
msharmavikram <vsm2@illinois.edu>
2020-02-25 18:04:19 -0600

    updates

---
+++ common/bafs_error.h
@@ -0,0 +1,11 @@
+#ifndef __BAFS_ERRORS__
+#define __BAFS_ERRORS__
+
+#include <stdint.h>
+#include <bafs_utils.h>
+
+
+
+
+
+#endif //__BAFS_ERRORS__
--- common/bafs_utils.h
+++ common/bafs_utils.h
@@ -14,12 +14,25 @@
 /*Extract range of bits from register/value*/
 #define _RDBITS(val, high, low) (((val) & _RBIT_MASK((high),(low))) >> (lo))
 
+//TODO: Can this be converted to a constexpr?
+/*Read register value*/
+//example: Read ptr[31:16] --> _REG(ptr, 2, 16) -- 2 is the start position of offset and 16 is size of read
+#define _REG(regptr, offset, type) \
+    ((volatile uint##type##_t *) (((volatile unsigned char *) ((volatile void*)(regptr))) + (offset)))
 
-/*Standard fields */
 
+/*Common feilds across different commands */
+//Assumption: ptr is base ptr of the cmd struct
+#define BAFS_IOCMD_CID(ptr)     _REG(ptr, 2, 16) 
+#define BAFS_IOCMD_NSID(ptr)    _REG(ptr, 4, 32)
 
-
-
-
+//completion queue ptrs
+//sq head ptr in CQ element
+#define BAFS_CQ_SQHP(ptr)   _REG(ptr,  8, 16)
+#define BAFS_CQ_SQID(ptr)   _REG(ptr, 10, 16)
+#define BAFS_CQ_CID(ptr)    _REG(ptr, 12, 16) //This is in DW3
+// this is status field
+#define BAFS_CQ_STS(ptr)    _REG(ptr, 14, 16)
+#define BAFS_CQ_PHASE(ptr) (_REG(ptr, 14, 16) & 0x1) //this is phase bit
 
 #endif //__BAFS_UTILS_H_
